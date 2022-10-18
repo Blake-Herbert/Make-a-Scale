6202edd2075fa83df98a0a1f4a75ab4e40f5579a
Blake-Herbert <112880385+Blake-Herbert@users.noreply.github.com>
2022-10-18 19:18:32 -0400

    Add files via upload

---
+++ Music Scales/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] allDiatonicNotes = { "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B" };
+
+int[] majorScaleIntervals = { 2, 2, 1, 2, 2, 2, 1 };
+
+int selectedKey = 0;
+Console.WriteLine(allDiatonicNotes[selectedKey]);
+selectedKey += majorScaleIntervals[0];
+Console.WriteLine(allDiatonicNotes[selectedKey]);
+selectedKey += majorScaleIntervals[1];
+Console.WriteLine(allDiatonicNotes[selectedKey]);
+selectedKey += majorScaleIntervals[2];
+Console.WriteLine(allDiatonicNotes[selectedKey]);
+selectedKey += majorScaleIntervals[3];
+Console.WriteLine(allDiatonicNotes[selectedKey]);
+selectedKey += majorScaleIntervals[4];
+Console.WriteLine(allDiatonicNotes[selectedKey]);
+selectedKey += majorScaleIntervals[5];
+Console.WriteLine(allDiatonicNotes[selectedKey]);
